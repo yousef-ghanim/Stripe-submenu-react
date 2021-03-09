62cb1b03afe9c488f0a2ea6a61bda9747bba9ed4
yousef-ghanim <yousefghanim85@gmail.com>
2021-03-09 17:14:57 +0000

    fixing format

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,53 +1,52 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars, FaGripVertical } from 'react-icons/fa'
-import {useGlobalContext} from './context'
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars, FaGripVertical } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Navbar = () => {
-  const {openSidebar, openSubmenu, closeSubmenu} = useGlobalContext();
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
   const displaySubmenu = (e) => {
     const page = e.target.textContent;
     const tempBtn = e.target.getBoundingClientRect();
-    const center = (tempBtn.left + tempBtn.right)/ 2;
+    const center = (tempBtn.left + tempBtn.right) / 2;
     const bottom = tempBtn.bottom - 3;
-    openSubmenu(page, {center, bottom});
-  }
+    openSubmenu(page, { center, bottom });
+  };
   const handleSubmenu = (e) => {
-    if(!e.target.classList.contains('link-btn')){
-       closeSubmenu()
+    if (!e.target.classList.contains("link-btn")) {
+      closeSubmenu();
     }
-    
-  }
+  };
   return (
-  <nav className='nav' onMouseOver={handleSubmenu}>
-    <div className="nav-center">
-      <div className="nav-header">
-        <img src={logo} className='nav-logo' alt="stripe"/>
-        <button className='btn toggle-btn'onClick={openSidebar}>
-          <FaBars />
-        </button>
+    <nav className="nav" onMouseOver={handleSubmenu}>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="stripe" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              products
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              developers
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
+          </li>
+        </ul>
+        <button className="btn signin-btn">Sign in</button>
       </div>
-      <ul className='nav-links'>
-        <li>
-          <button className='link-btn' onMouseOver={displaySubmenu}>
-            products
-          </button>
-        </li>
-        <li>
-          <button className='link-btn' onMouseOver={displaySubmenu}>
-            developers
-          </button>
-        </li>
-        <li>
-          <button className='link-btn' onMouseOver={displaySubmenu}>
-            company
-          </button>
-        </li>
-      </ul>
-      <button className='btn signin-btn'>Sign in</button>
-    </div>
-  </nav>
-  )
-}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
