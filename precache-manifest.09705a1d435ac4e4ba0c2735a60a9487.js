b3157b3dcd129901c4467a6281f55eac2a774582
yousef-ghanim <yousefghanim85@gmail.com>
2021-04-12 06:10:34 +0000

    Updates

---
+++ precache-manifest.09705a1d435ac4e4ba0c2735a60a9487.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b73fda204d6629bf498494ee72019d",
+    "url": "/stripe-submenu-react/index.html"
+  },
+  {
+    "revision": "a9eed19aab1c41a470b3",
+    "url": "/stripe-submenu-react/static/css/main.990a8c41.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/stripe-submenu-react/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stripe-submenu-react/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9eed19aab1c41a470b3",
+    "url": "/stripe-submenu-react/static/js/main.4205bcc9.chunk.js"
+  },
+  {
+    "revision": "21714b126ef5a48f5304",
+    "url": "/stripe-submenu-react/static/js/runtime-main.627c99b9.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/stripe-submenu-react/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/stripe-submenu-react/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/stripe-submenu-react/static/media/phone.58d7e3d6.svg"
+  }
+]);
